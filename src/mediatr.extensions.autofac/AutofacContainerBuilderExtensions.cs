bacd8934beec4b5e4fb6cd0117cc4ae44e1ffac1
Sean Drumm <sean.drumm@gmail.com>
2016-11-10 11:56:57 +0000

    Init

---
+++ src/mediatr.extensions.autofac/AutofacContainerBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace MediatR
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Autofac;
+    using Autofac.Features.Variance;
+
+    public static class AutofacContainerBuilderExtensions
+    {
+        public static void AddMediatR(this ContainerBuilder builder, Assembly assembly)
+        {
+            Decorate(builder, assembly);
+        }
+
+        private static void RegisterRequestHandlersFromAssembly(ContainerBuilder builder, Assembly assembly)
+        {
+            builder.RegisterAssemblyTypes(assembly).As(t => t.GetTypeInfo().GetInterfaces()
+                .Where(i => i.IsClosedTypeOf(typeof(IRequestHandler<,>))));
+        }
+
+        private static void RegisterAsyncRequestHandlersFromAssembly(ContainerBuilder builder, Assembly assembly)
+        {
+            builder.RegisterAssemblyTypes(assembly).As(t => t.GetTypeInfo().GetInterfaces()
+                .Where(i => i.IsClosedTypeOf(typeof(IAsyncRequestHandler<,>))));
+        }
+
+        private static void RegisterNotificationHandlersFromAssembly(ContainerBuilder builder, Assembly assembly)
+        {
+            builder.RegisterAssemblyTypes(assembly)
+                .As(t => t.GetTypeInfo().GetInterfaces().Where(i => i.IsClosedTypeOf(typeof(INotificationHandler<>))).ToArray());
+        }
+
+        private static void RegisterAsyncNotificationHandlersFromAssembly(ContainerBuilder builder, Assembly assembly)
+        {
+            builder.RegisterAssemblyTypes(assembly)
+                .As(t => t.GetTypeInfo().GetInterfaces().Where(i => i.IsClosedTypeOf(typeof(IAsyncNotificationHandler<>))).ToArray());
+        }
+
+        public static void Decorate(ContainerBuilder builder, Assembly handlersAssembly)
+        {
+            builder.RegisterSource(new ContravariantRegistrationSource());
+            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly).AsImplementedInterfaces();
+            builder.Register<SingleInstanceFactory>(ctx =>
+            {
+                var c = ctx.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+            builder.Register<MultiInstanceFactory>(ctx =>
+            {
+                var c = ctx.Resolve<IComponentContext>();
+                return t => (IEnumerable<object>)c.Resolve(typeof(IEnumerable<>).MakeGenericType(t));
+            });
+
+            RegisterRequestHandlersFromAssembly(builder, handlersAssembly);
+            RegisterAsyncRequestHandlersFromAssembly(builder, handlersAssembly);
+            RegisterNotificationHandlersFromAssembly(builder, handlersAssembly);
+            RegisterAsyncNotificationHandlersFromAssembly(builder, handlersAssembly);
+        }
+    }
+}
---
+++ test/mediatr.extensions.autofac.tests/Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace mediatr.extensions.autofac.tests
+{
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Autofac;
+    using MediatR;
+    using Xunit;
+
+    public class Tests
+    {
+        [Fact]
+        public async Task TestResolve()
+        {
+            var builder = new ContainerBuilder();
+            builder.AddMediatR(typeof(Tests).GetTypeInfo().Assembly);
+            var container = builder.Build();
+
+            var handler = container.Resolve<IAsyncRequestHandler<Ping, Unit>>();
+            await handler.Handle(new Ping { Name = "Bob" });
+        }
+
+        [Fact]
+        public async Task TestMediator()
+        {
+            var builder = new ContainerBuilder();
+            builder.AddMediatR(typeof(Tests).GetTypeInfo().Assembly);
+            var container = builder.Build();
+            var mediator = container.Resolve<IMediator>();
+            await mediator.SendAsync(new Ping { Name = "Bob" });
+        }
+    }
+
+    public class Ping : IAsyncRequest
+    {
+        public string Name { get; set; }
+    }
+
+    public class PingHandler : IAsyncRequestHandler<Ping, Unit>
+    {
+        public Task<Unit> Handle(Ping message)
+        {
+            return Task.FromResult(Unit.Value);
+        }
+    }
+}
